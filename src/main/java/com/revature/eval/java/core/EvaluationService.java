e2fbaf2c3d6a12cc464be54642783d377dd5d0b3
Travis Taylor <travis795@revature.net>
2022-06-02 20:23:28 -0400

    Solved question #7

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -184,7 +184,30 @@ public class EvaluationService {
 	 */
 	public String printNumberInWord(int number) {
 		// TODO Write an implementation for this method declaration
-		return null;
+		switch (number){
+			case 0:
+				return "ZERO";
+			case 1:
+				return "ONE";
+			case 2:
+				return "TWO";
+			case 3: 
+				return "THREE";
+			case 4:
+				return "FOUR";
+			case 5:
+				return "FIVE";
+			case 6: 
+				return "SIX";
+			case 7:
+				return "SEVEN";
+			case 8: 
+				return "EIGHT";
+			case 9:
+				return "NINE";
+			default: 
+				return "OTHER";
+		}
 	}
 
 	/**
